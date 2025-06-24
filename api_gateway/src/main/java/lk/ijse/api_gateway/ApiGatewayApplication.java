086ce37ee948002357c0ce699047f2f3268c81ed
RasanduniLakmali <rasandunilakmali26@gmail.com>
2025-06-24 23:56:50 +0530

    api-gateway updated

--- api_gateway/src/main/java/lk/ijse/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/lk/ijse/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,30 @@
 package lk.ijse.api_gateway;
 
+import lk.ijse.api_gateway.config.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder, JwtFilter jwtFilter) {
+        return builder.routes()
+                .route("user-service-protected", r -> r.path("/user-service/**")
+                        .and()
+                        .predicate(exchange -> !exchange.getRequest().getURI().getPath().contains("/register")
+                                && !exchange.getRequest().getURI().getPath().contains("/login"))
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("http://localhost:8081"))
+
+                .build();
     }
 
 }
